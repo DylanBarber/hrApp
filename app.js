5f7fff301956a4e4d3b686da5dce72aec233a1b3
Dylan Barber <DylanBarber1@Outlook.com>
2019-09-13 12:52:34 -0400

    Create function for GETting all users from database

--- app.js
+++ app.js
@@ -10,55 +10,16 @@ const sql = mysql.createConnection({
   database: "users"
 });
 sql.connect();
-// app.get("/users", (req, res) => {
-  
-// });
-for (let i = 1; i < 101; i++) {
-  setTimeout(() => {
+app.get("/users", (req, res) => {
+  sql.query("SELECT * FROM USERS", (err, rows) => {
+    if (err) throw err;
+    res.send(rows);
+  });
+});
 
+const port = process.env.PORT || 25565;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 
-
-    const fetchUsers = async () => {
-      const response = await fetch("https://randomuser.me/api/?nat=us");
-      const body = await response.json();
-      if (response.status !== 200) {
-        throw Error(body.message);
-      }
-      const userObj = {
-        fname: body.results[0].name.first,
-        lname: body.results[0].name.last,
-        email: body.results[0].email,
-        phone: body.results[0].phone,
-        street: body.results[0].location.street,
-        city: body.results[0].location.city,
-        state: body.results[0].location.state,
-        dob: body.results[0].dob.date,
-        hireDate: body.results[0].registered.date
-
-
-      };
-
-      sql.query(`INSERT INTO users (fname, lname, email, phone, street, city, state, dob, hireDate) VALUES ('${userObj.fname}', '${userObj.lname}', '${userObj.email}', '${userObj.phone}', '${userObj.street}', '${userObj.city}', '${userObj.state}', '${userObj.dob}', '${userObj.hireDate}')`);
-      // res.json(userObj);
-    };
-    fetchUsers();
-  }, 5000);
-}
-//Math.floor(Math.random() * 100000)
-
-// app.get("/users", (req, res) => {
-//   sql.connect();
-//   sql.query(`SELECT * FROM USERS WHERE fname='${req.query.fname}'`, (err, rows, fields) => {
-//     if (err) throw err;
-//     res.send(rows);
-//     sql.end();
-//     return;
-//   });
-// });
-// sql.end();
-// const port = process.env.PORT || 25565;
-// app.listen(port, () => {
-//   console.log(`Listening on port ${port}`);
-
-// });
+});
 
